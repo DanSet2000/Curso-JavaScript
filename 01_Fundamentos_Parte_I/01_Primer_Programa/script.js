008b52012ee3e22ea357b166b1b114b1d11142a1
Daniel Carvajal <danielcarva2000@hotmail.com>
2024-10-20 15:20:03 -0500

    Add Fundaments of JavaScript

---
+++ 01_Fundamentos_Parte_I/01_Primer_Programa/script.js
@@ -0,0 +1,6 @@
+let js = 'Hola';
+
+if (js === 'Hola') alert ('Bienvenido a mi Primera Pagina')
+
+// Imprimir en Consola el Código
+console.log(40 + 8);
---
+++ 01_Fundamentos_Parte_I/02_Valores_y_Variables/script.js
@@ -0,0 +1,32 @@
+// ---- Valores ----
+
+console.log("Daniel");
+console.log(23);
+
+// ---- Variables ----
+
+// Declaracion de Variables
+let firstName = 'Daniel';
+console.log(23);
+
+// Formas de Declarar Variables
+let edad = 30;                  // Puede mutar su valor
+edad = 31;
+
+const primerNombre = "Jonas";   // No cambia su valor
+
+
+// Tipos de Datos
+
+let age = 23;           // Number
+let name = 'Daniel';    // String
+let fullAge = true;     // Boolean
+let children;           // Undefined
+
+// Conocer el Tipo de Dato
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof(javaScriptIsFun));
+console.log(typeof(firstName));
+console.log(typeof(children));
+
