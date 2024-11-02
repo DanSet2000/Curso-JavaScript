20e5743d03df33eb2956248eacf3f88c0160c158
Daniel Carvajal <danielcarva2000@hotmail.com>
2024-11-02 13:28:51 -0500

    Add Iterations

--- 02_Fundamentos_Parte_II/04_Iteraciones/script.js
+++ 02_Fundamentos_Parte_II/04_Iteraciones/script.js
@@ -28,3 +28,39 @@ console.log(ages);
 
 // Recorrer una matriz de manera inversa
 
+for (let i = years.length - 1; i >= 0; i--){
+    console.log(years[i]);
+}
+
+// Un bucle for Anidado
+
+// Bucle indicando que se hacen 3 ejercicios
+for (let exercise = 1; exercise < 4; exercise++){
+    console.log('------- Iniciando Ejercicio ' + exercise)
+
+		// Bucle indicando que se realizan 5 repeticiones por cada ejercicio
+    for (let rep = 1; rep < 6; rep++){
+         console.log('Ejercicio: ' + exercise + ' Levantando peso en la Repeticion: ' + rep)
+    }
+}
+
+
+// ---- While ----
+
+// Usando el ciclo While
+let rep = 1;
+
+while (rep <= 10){
+    console.log('Levantando peso en la repeticion: ' + rep);
+    rep++;
+}
+
+// Ciclo While sin usar un contador
+
+let dice = Math.trunc(Math.random() * 6) + 1
+
+while (dice !== 6){
+    console.log('Haz sacado un: ' + dice);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice ===6) console.log('El ciclo va a terminar, salió un 6')
+}
