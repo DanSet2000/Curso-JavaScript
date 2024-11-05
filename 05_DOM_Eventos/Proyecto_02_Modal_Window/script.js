b2776442abbd9ab923898bff22672b5a263ebb14
Daniel <danielcarva2000@hotmail.com>
2024-11-05 15:11:09 -0500

    Add Proyect #2 and #3

--- 05_DOM_Eventos/Proyecto_02_Modal_Window/script.js
+++ 05_DOM_Eventos/Proyecto_02_Modal_Window/script.js
@@ -4,14 +4,41 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.open-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// Funciones para Abrir y Cerrar el Modal
+const openModal = function(){
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
 
 // Usando QuerySelectorAll
 for (let i = 0; i < btnsOpenModal.length; i++){
-    console.log(btnsOpenModal[i].addEventListener('click', function(){
 
-    }));
+    btnsOpenModal[i].addEventListener('click', openModal);
+
+    btnCloseModal.addEventListener('click', closeModal);
+    overlay.addEventListener('click', closeModal);
+
+    // Eventos de Teclado
+    document.addEventListener('keydown', function(e){
+        console.log(e.key);
+
+        if (e.key === 'Escape'){
+            if (!modal.classList.contains('hidden')){
+                closeModal();
+            }
+        }
+    })
 }
 
-// Trabajando con Clases
+
+
+
 
