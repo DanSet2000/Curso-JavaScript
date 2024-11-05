7cbe815df94ee19c0196c31902ee5d9d12acb285
Daniel Carvajal <danielcarva2000@hotmail.com>
2024-11-05 09:03:35 -0500

    Add Proyect

--- 05_DOM_Eventos/Proyecto_01_Guess_My_Number/script.js
+++ 05_DOM_Eventos/Proyecto_01_Guess_My_Number/script.js
@@ -1,10 +1,15 @@
 'use strict';
 
-// Selector: Mismo selector que usariamos en CSS
+/* Selector: Mismo selector que usariamos en CSS
+   Manejo de Eventos
+   Escuchar un evento, pasar el tipo de evento e indicar que debe hacer */
 
-// Manejo de Eventos
+// Generar un numero aleatorio para el juego entre 1 y 20
+const secretNumber = Math.trunc((Math.random()*20) + 1);
+document.querySelector('.number').textContent = secretNumber;
 
-// Escuchar un evento, pasar el tipo de evento e indicar que debe hacer
+// Reducir el puntaje con respuestas incorrectas
+let score = 20;
 
 // Funcion que revisa si hay un numero al presionar el boton de Check
 document.querySelector('.check').addEventListener('click', function(){
@@ -12,7 +17,29 @@ document.querySelector('.check').addEventListener('click', function(){
 
     console.log(guess)
 
+    // Comparar el numero con el del usuario
     if (!guess){
         document.querySelector('.message').textContent = '⛔ No number!';
-    }
+    } else if (guess === secretNumber){
+        document.querySelector('.message').textContent = '🥳 Correct Number!';
+    } else if (guess > secretNumber){
+        if (score > 1){
+            document.querySelector('.message').textContent = '📈 Too High!';
+            score--;    // Reducir Score
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '💣 You Lost the Game!';
+            document.querySelector('.score').textContent = 0;
+        }
+    } else if (guess < secretNumber){
+        if (score > 1){
+            document.querySelector('.message').textContent = '📉 Too Low!';
+            score--;    // Reducir Score
+            document.querySelector('.score').textContent = score;
+        }else {
+            document.querySelector('.message').textContent = '💣 You Lost the Game!';
+            document.querySelector('.score').textContent = 0;
+        }
+    } 
 });
+
