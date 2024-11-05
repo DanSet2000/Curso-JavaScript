b9327ac800b6076291222211a8c5388672a8191e
Daniel <danielcarva2000@hotmail.com>
2024-11-04 21:04:22 -0500

    Add DOM Events into Proyect 1

--- 05_DOM_Eventos/Proyecto_01_Guess_My_Number/script.js
+++ 05_DOM_Eventos/Proyecto_01_Guess_My_Number/script.js
@@ -1,4 +1,18 @@
 'use strict';
 
 // Selector: Mismo selector que usariamos en CSS
-document.querySelector('.message')
+
+// Manejo de Eventos
+
+// Escuchar un evento, pasar el tipo de evento e indicar que debe hacer
+
+// Funcion que revisa si hay un numero al presionar el boton de Check
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+
+    console.log(guess)
+
+    if (!guess){
+        document.querySelector('.message').textContent = '⛔ No number!';
+    }
+});
