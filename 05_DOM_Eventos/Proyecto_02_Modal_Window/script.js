75e2a414f5ca6540fb95768b757ce9f514fbbd36
Daniel Carvajal <danielcarva2000@hotmail.com>
2024-11-05 11:52:40 -0500

    Add Proyect 2 - Modal Window

---
+++ 05_DOM_Eventos/Proyecto_02_Modal_Window/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Guardando el contenido en una variable
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.open-modal');
+
+// Usando QuerySelectorAll
+for (let i = 0; i < btnsOpenModal.length; i++){
+    console.log(btnsOpenModal[i].addEventListener('click', function(){
+
+    }));
+}
+
+// Trabajando con Clases
+
