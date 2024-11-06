641ffcac16274b2c8e2ee1fd433f64c7c331b3a8
Daniel Carvajal <danielcarva2000@hotmail.com>
2024-11-05 21:57:56 -0500

    Finish Proyect 03

--- 05_DOM_Eventos/Proyecto_03_Pig_Game/script.js
+++ 05_DOM_Eventos/Proyecto_03_Pig_Game/script.js
@@ -12,37 +12,89 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Condiciones de Inicio
-score0Element.textContent = 0;
-score1Element.textContent = 0;
-diceElement.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+// Funciones para Refactorizar
+function swithPlayer(){
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
+}
+
+const init = function(){
+    // Condiciones de Inicio
+    diceElement.classList.add('hidden');
+
+    const scores = [0, 0];
+    let currentScore = 0;
+    let activePlayer = 0;
+    let playing = true;
+
+    // Seteando todo de nuevo
+    score0Element.textContent = 0;
+    score1Element.textContent = 0;
+    current0Element.textContent = 0;
+    current1Element.textContent = 0;
+    
+    player0Element.classList.remove('player--winner');
+    player1Element.classList.remove('player--winner');
+    player0Element.classList.add('player--active');
+    player1Element.classList.remove('player--active');
+}
+
+init();
 
 // Funcionalidad de Rodar el Dado
 
 btnRoll.addEventListener('click', function(){
-    // 1. Generar un numero aleatorio para el dado
-    const dice = Math.trunc(Math.random() * 6) + 1;
 
-    // 2. Mostrar el dado
-    diceElement.classList.remove('hidden');
-    diceElement.src = `dice-${dice}.png`    // Cargar la imagen del Dado
+    if (playing){
+        // 1. Generar un numero aleatorio para el dado
+        const dice = Math.trunc(Math.random() * 6) + 1;
 
-    // 3. Revisar si el dado cayó en 1
-    if (dice !== 1){
-        // Agregar Dice al Score
-        currentScore = currentScore + dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-    }else{
-        // Cambiar al otro jugador
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        currentScore = 0;
-        player0Element.classList.toggle('player--active');
-        player1Element.classList.toggle('player--active');
+        // 2. Mostrar el dado
+        diceElement.classList.remove('hidden');
+        diceElement.src = `dice-${dice}.png`    // Cargar la imagen del Dado
+
+        // 3. Revisar si el dado cayó en 1
+        if (dice !== 1){
+            // Agregar Dice al Score
+            currentScore = currentScore + dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        }else{
+            // Cambiar al otro jugador
+            swithPlayer();
+        }
     }
 
+
 })
+
+// Funcionalidad para mantener el Score
+
+btnHold.addEventListener('click', function(){
+
+    if (playing){
+        // 1. Agregar el Score actual al del Jugador
+        scores[activePlayer] += currentScore;
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+        // 2. Revisar si el Score del jugador es >= 100
+        if (scores[activePlayer] >= 100){
+            // Terminar el Juego
+            playing = false;
+            diceElement.classList.add('hidden');
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--active');
+        }else{
+            // Pasar al otro Jugador
+            swithPlayer();
+        }
+    }
+})
+
+// Funcionalidad de Nuevo Juego
+
+btnNew.addEventListener('click', init)
