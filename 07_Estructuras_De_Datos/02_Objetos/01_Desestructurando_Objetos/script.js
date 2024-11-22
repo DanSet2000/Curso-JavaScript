fba1be9c32a62f1c72aaefdfc04f24a7a1e0c7dd
Daniel Carvajal <danielcarva2000@hotmail.com>
2024-11-22 18:47:21 -0500

    Add Spread Operator

--- 07_Estructuras_De_Datos/02_Objetos/01_Desestructurando_Objetos/script.js
+++ 07_Estructuras_De_Datos/02_Objetos/01_Desestructurando_Objetos/script.js
@@ -8,7 +8,7 @@ const restaurant = {
     name: 'Classico Italiano',
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
     categories: ['Italian', 'Pizzeria', 'Vegatarian', 'Organic'],
-    startedMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
     openingHours: {
         thu: {
@@ -26,7 +26,57 @@ const restaurant = {
     },
 
     order: function(starterIndex, mainIndex){
-        return [this.startedMenu[starterIndex], this.mainMenu[mainIndex]];
-    }
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function({
+        starterIndex = 1, 
+        mainIndex = 0, 
+        time = '20:00', 
+        address}){
+        console.log(`Order Received: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
 };
 
+// Definir algunas variables y escribir exactamente los nombres de las propiedades para extraerlos
+
+// Desestructurando el Objeto Restaurante
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+// Usando nombres diferentes para las variables
+// Nombre Actual: Nuevo Nombre
+const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Establecer valores predeterminados
+const { menu = [],  starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutacion de Variables al Desestructurar Objetos
+let a = 111;
+let b = 999;
+const object = {a: 23, b: 7, c: 14};
+
+//{a, b} = obj; -- Error de Sintaxis
+({ a, b } = object);   // a = 23   b = 7
+console.log(a, b);
+
+// Objetos Anidados
+const {
+    fri: { open, close },
+} = openingHours;
+console.log(open, close);
+
+// Ejercicio de Funcion
+restaurant.orderDelivery({
+    time: '22:30',
+    address: 'Via del Sol, 21',
+    mainIndex: 2,
+    startedIndex: 2,
+});
+
+restaurant.orderDelivery({
+    address: 'Via del Sol, 21',
+    startedIndex: 1,
+});
---
+++ 07_Estructuras_De_Datos/02_Objetos/02_Spread_Operador/script.js
@@ -0,0 +1,3 @@
+'use strict';
+
+// ---- Spread Operator ----
