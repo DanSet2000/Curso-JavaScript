93a09d1222b952f7b31bdbd08c447e58704956db
Daniel Carvajal <danielcarva2000@hotmail.com>
2024-12-16 19:55:43 -0500

    Add Extra Methods

--- 07_Estructuras_De_Datos/05_Strings/03_Tercera_Parte/script.js
+++ 07_Estructuras_De_Datos/05_Strings/03_Tercera_Parte/script.js
@@ -9,5 +9,25 @@ console.log('Daniel Carvajal'.split(' '));
 // Creando variables directamente 
 const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
 
-const newName = ['Mr. ', firstName, lastName.toUpperCase].join(' ');
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
 console.log(newName);
+
+// Ejemplo
+
+const capitalizeName = function(name){
+    const names = name.split(' ');  // Separando Nombres
+    const namesUpper = [];
+
+    for(const word of names){
+        namesUpper.push(word[0].toUpperCase() + word.slice(1));      // Primer Letra de Cada Nombre en Mayuscula
+    }
+
+    console.log(namesUpper.join(' '));
+}
+
+capitalizeName('jessica ann smith davis');
+
+// Llenando una cadena
+const message = 'Go to gate 23!';
+console.log(message.padStart(25, '+'));     // Longitud y Caracter a usar
+console.log(message.padEnd(30, '$'))
