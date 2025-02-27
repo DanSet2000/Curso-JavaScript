59c6b4219137351399a5380cb868866708eb4ef4
Daniel Carvajal <danielcarva2000@hotmail.com>
2025-02-26 21:55:34 -0500

    Add Working with Arrays Classes

--- 09_Trabajando_con_Arreglos/05_Metodos_Adicionales/script.js
+++ 09_Trabajando_con_Arreglos/05_Metodos_Adicionales/script.js
@@ -12,3 +12,22 @@ const firstWitdrawal = movements.find(function(movement){
     return movement < 0;
 })
 console.log(firstWitdrawal);
+
+// ---- FIND LAST ----
+
+// Determinar el ultimo retiro: -130
+
+const lastWitdrawal = movements.findLast(function(movement){
+    return movement < 0;
+});
+
+console.log(lastWitdrawal);
+
+// ---- FIND LAST INDEX ----
+
+const latestLargeMovementIndex = movements.findLastIndex(function(movement){
+    return Math.abs(movement) > 1000;
+})
+
+console.log(latestLargeMovementIndex);
+console.log(`Your latest large movement was ${movements.length - latestLargeMovementIndex} movements ago`);
